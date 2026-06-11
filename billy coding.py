8d7a4ed9798097f8b0ff236e824491e0ad0c1970
Billy Zhou <zhoubilly123@gmail.com>
2026-06-10 17:40:08 -0700

    Update billy coding.py

--- billy coding.py
+++ billy coding.py
@@ -1,4 +1,4 @@
-﻿import random
+import random
 print("This is a dnd game\n")
 print("This is also my first project \n")
 username = input("----What is your name?----\n ")
@@ -7,62 +7,45 @@ if answer == "Yes".strip().lower():
     print("Let's get started")
 else:
     print("Too bad")
-class Brawler():
-    def __init__(self):
-        self.name = username
-        self.damage = random.randint(20,25)
-        self.health = random.randint(110,120)
-        self.heal = 10
-class Mage():
-    def __init__(self):
-        self.name = username
-        self.damage = random.randint(30,35)
-        self.health = random.randint(50,60)
-        self.heal = 12
-class Archer():
-    def __init__(self):
-        self.name = username
-        self.damage = random.randint(40,45)
-        self.health = random.randint(80,100)
-        self.heal = 9
-class Test():
-    def __init__(self):
-        self.name = username
-        self.damage = random.randint(1000,1000)
-        self.health = random.randint(8000,10000)
-        self.heal = 22
-class Bard():
-    def __init__(self):
-        self.name = username
-        self.damage = random.randint(20,25)
-        self.health = random.randint(80,90)
-        self.heal = 20
-class Beast():
-    def __init__(self):
-        self.name = username
-        self.damage = random.randint(30,35)
-        self.health = random.randint(100,120)
-        self.heal = 10
+level = 0
+Area = ["Forest, Dungeon, Cave"]
 
 
+
+class Default():
+    def __init__(self, classes, damage, health, heal):
+        self.name = username
+        self.classes = classes
+        self.damage = damage
+        self.health = health
+        self.heal = heal
+
+def attack():
+    current_enemy.health -= player.damage
+    print(f"You did {player.damage} to {current_enemy.health}")
+
+def heal():
+    player.health += player.heal
+    print(f"you healed for {player.heal}, and your current hp is {player.health}")
+
+        
 choice = input("what class do you want. ")
 if choice == "Brawler".strip().lower():
-    player = Brawler()
+    player = Default("Brawler", 25, 120, 8)
 if choice == "Mage".strip().lower():
-    player = Mage()
+    player = Default("Mage", 30, 80, 8)
 if choice == "Archer".strip().lower():
-    player = Archer()
-if choice == "Test".strip().lower():
-    player = Test()
+    player = Default("Archer", 10, 200, 13)
 if choice == "Bard".strip().lower():
-    player = Test()
+    player = Default("Bard", 5, 100, 20)
+
 
 class enemy():
     def __init__(self, name, health, damage):
         self.name = name
         self.health = health
         self.damage = damage
-enemy_enemy = random.randint(1,3)
+enemy_enemy = random.randint(1,5)
 if enemy_enemy == 1:
     current_enemy = enemy("Goblin", 80, 10)
 elif enemy_enemy == 2:
@@ -70,24 +53,32 @@ elif enemy_enemy == 2:
 elif enemy_enemy == 3:
     current_enemy = enemy("Adam", 150, 12)
 
-print("you entered the castle.")
-while True:
-    print(f"{current_enemy} has appeared")
-    while current_enemy.health > 0:
-        enemy_action = random.randint(2,3)
-        action = input("Actions: Attack, Heal, Flee \n" )
-        if action == "Attack".strip().lower():
-            current_enemy.health -= player.damage
-            print(f"You did {player.damage} now the enemy has {current_enemy.health} ")
-        if action == "Heal".strip().lower():
-            player.health += player.heal
-            print(f"You healed for {player.heal}. Your current health is {player.health}")
-        if enemy_action <= 2:
-            player.health -= current_enemy.damage
-            print(f"The enemy did {current_enemy.damage} to you and your hp is now {player.health}")
-        if current_enemy.health <= 0:
-            print("You defeated the enemy")
-            break
-        if player.health == 0:
-            print(f"Stand proud {player.name} you are strong")
-            break
+
+print("You entered the caste")
+while player.health > 0:
+    decision = input("Where do you want to go next? Options: Continue, Forest, Desert ")
+    if decision.strip().lower() == "Continue":
+        while current_enemy.health > 0:
+            enemy_action = random.randint(1,3)
+            print(f"{current_enemy.name} has appeared")
+            action = input("Actions: Attack, Heal, Flee \n" )
+            if action == "Attack".strip().lower():
+                attack()
+            elif action == "Heal".strip().lower():
+                player.health += player.heal
+                print(f"You healed for {player.heal}. Your current health is {player.health}")
+            if enemy_action <= 2:
+                player.health -= current_enemy.damage
+                print(f"The enemy did {current_enemy.damage} to you and your hp is now {player.health}")
+            elif enemy_action == 2:
+                current_enemy.health += current_enemy.heal
+                print(f"{current_enemy.name} has healed for {current_enemy.heal} and now its hp is {current_enemy.health}")
+            if current_enemy.health <= 0:
+                print("You defeated the enemy")
+                level += 1
+                print(f"{player.name} is now level {level}")
+                reward = input("The ")
+                break
+    
+       
+
