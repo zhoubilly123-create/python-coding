2555a8db8f4b1d9b1c799ac6fcabd125cb529c77
zhoubilly123-create <zhoubilly123@gmail.com>
2026-06-20 15:40:48 -0700

    Refactor combat functions and enemy interactions
    Refactor combat mechanics and restore attack/heal functions

--- billy coding.py
+++ billy coding.py
@@ -9,6 +9,7 @@ else:
     print("Too bad")
 level = 0
 Area = ["Forest, Dungeon, Cave"]
+inventory = [""]
 
 
 
@@ -19,16 +20,7 @@ class Default():
         self.damage = damage
         self.health = health
         self.heal = heal
-
-def attack():
-    current_enemy.health -= player.damage
-    print(f"You did {player.damage} to {current_enemy.health}")
-
-def heal():
-    player.health += player.heal
-    print(f"you healed for {player.heal}, and your current hp is {player.health}")
-
-        
+        self.inventory = inventory
 choice = input("what class do you want. ")
 if choice == "Brawler".strip().lower():
     player = Default("Brawler", 25, 120, 8)
@@ -39,46 +31,63 @@ if choice == "Archer".strip().lower():
 if choice == "Bard".strip().lower():
     player = Default("Bard", 5, 100, 20)
 
-
 class enemy():
     def __init__(self, name, health, damage):
         self.name = name
         self.health = health
         self.damage = damage
-enemy_enemy = random.randint(1,5)
+enemy_enemy = random.randint(1,3)
 if enemy_enemy == 1:
     current_enemy = enemy("Goblin", 80, 10)
 elif enemy_enemy == 2:
     current_enemy = enemy("Knight", 120, 15)
 elif enemy_enemy == 3:
     current_enemy = enemy("Adam", 150, 12)
+    
 
+def attack():
+    current_enemy.health -= player.damage
+    print(f"You did {player.damage} to {current_enemy.health}")
 
-print("You entered the caste")
-while player.health > 0:
-    decision = input("Where do you want to go next? Options: Continue, Forest, Desert ")
-    if decision.strip().lower() == "Continue":
-        while current_enemy.health > 0:
+def heal():
+    player.health += player.heal
+    print(f"you healed for {player.heal}, and your current hp is {player.health}")
+def combat():
+    print(f"{current_enemy.name} has appeared")
+    while current_enemy.health > 0 and player.health > 0:
             enemy_action = random.randint(1,3)
-            print(f"{current_enemy.name} has appeared")
             action = input("Actions: Attack, Heal, Flee \n" )
             if action == "Attack".strip().lower():
                 attack()
             elif action == "Heal".strip().lower():
                 player.health += player.heal
                 print(f"You healed for {player.heal}. Your current health is {player.health}")
+            else:
+                print("invalid action")
             if enemy_action <= 2:
                 player.health -= current_enemy.damage
                 print(f"The enemy did {current_enemy.damage} to you and your hp is now {player.health}")
-            elif enemy_action == 2:
+            elif enemy_action > 2:
                 current_enemy.health += current_enemy.heal
                 print(f"{current_enemy.name} has healed for {current_enemy.heal} and now its hp is {current_enemy.health}")
             if current_enemy.health <= 0:
                 print("You defeated the enemy")
                 level += 1
                 print(f"{player.name} is now level {level}")
-                reward = input("The ")
+                reward = input("you got some gold")
                 break
+            if player.health == 0:
+                print("game over")
+                break
+
+
+
+
+print("You entered the caste")
+while True:
+    decision = input("Where do you want to go next? Options: Continue, Forest, Desert ")
+    if decision.strip().lower() == "Continue":
+        combat()
     
        
 
